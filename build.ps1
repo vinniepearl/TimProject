0a911eab380bca842fcff24b3b0db306a74693c6
vinniepearl <55268968+vinniepearl@users.noreply.github.com>
2020-06-01 11:05:30 -0500

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,1 +1,9 @@
+[CmdletBinding()]
+param (
+[Parameter()]
+[Validateset("Init","Clean","Staging","ImportStagingModule","Test","UpdateVersion")]
+[System.String[]]
+$TaskList = 'Default'
+)
+
 write-host "Build Task"
